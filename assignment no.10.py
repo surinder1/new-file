97e0aa1602f1e0d8ae84f4621584eea0ad12ca17
surinder055 <surinder055jk@gmail.com>
2018-06-25 23:01:28 +0530

    2 new files added

--- assignment no.10.py
+++ assignment no.10.py
@@ -1,4 +1,5 @@
 ##
+#Q.1- Create a circle class and initialize it with radius. Make two methods getArea and getCircumference inside this class.
 
 class circle:
 	def __init__(self,radius):
@@ -6,6 +7,7 @@ class circle:
 	 
 	def get_area(self):
 		print(3.14*self.r*self.r)
+		
 	def get_circumfrance(self):
 		print(2*3.14*self.r)
 
@@ -15,6 +17,10 @@ a.get_circumfrance()
 
 
 ##
+#Q.2- Create a Student class and initialize it with name and roll number. Make methods to :
+#1. Display - It should display all informations of the student.
+
+
 class student():
 	def __init__(self,name,roll_number):
 		self.name=name
@@ -29,7 +35,9 @@ a.display()
 
 
 ###
-
+#Q.3- Create a Temprature class. Make two methods :
+#1. convertFahrenheit - It will take celsius and will print it into Fahrenheit.
+#2. convertCelsius - It will take Fahrenheit and will convert it into Celsius.
 class temperature():
 	def __init__(self,fahrenheit,celsius):
 		self.fahrenheit=fahrenheit
@@ -45,3 +53,56 @@ class temperature():
 a=temperature(int(input("fahrenheit:")),int(input("fahrenheit:")))
 a.convertcelsius()
 a.convertfahrenhiet()
+
+###
+#Q.4- Create a class MovieDetails and initialize it with Movie name, artistname,Year of release and ratings .
+#Make methods to 
+#1. Display-Display the details.
+#2. Update- Update the movie details.
+
+class movieDetail():
+	def __init__(self,moviename,artistname,year_of_release,rating):
+		self.moviename=moviename
+		self.artistname=artistname
+		self.year_of_release=year_of_release
+		self.rating=rating
+		
+	def display(self):
+		print("movie name is:",self.moviename)
+		print("artistname name is:",self.artistname)
+		print("year of release is:",self.year_of_release)
+		print("rating is:",self.rating)
+		
+	def update(self,moviename,artistname,year_of_release,rating):
+		self.moviename=moviename
+		self.artistname=artistname
+		self.year_of_release=year_of_release
+		self.rating=rating
+		
+t=movieDetail(input("enter any moviename:"),input("enter the name of artist:"),input("enter the release date:"),input("enter the rating of movie"))
+t.display()
+t.update(input("enter any moviename:"),input("enter the name of artist:"),input("enter the release date:"),input("enter the rating of movie"))
+t.display()
+
+
+#Q.5- Create a class Expenditure and initialize it with expenditure,savings.Make the following methods. 
+# 1. Display expenditure and savings 
+# 2. Calculate total salary
+# 3. Display salary
+
+class expenditure():
+	def __init__(self,expenditure,saving):
+			self.expenditure=expenditure
+			self.saving=saving
+	
+	def display(self):
+		print("enter monthly expenditure",self.expenditure)
+		print("enter monthly saving",self.saving)
+		
+	def calculate(self):
+		self.salary=self.expenditure+self.saving
+		print("your salary:",self.salary)
+
+a=expenditure(int(input("enter your expenditure:")),int(input("enter your savings:")))
+a.display()
+a.calculate()
---
+++ assignment no.11.py
@@ -0,0 +1,3 @@
+Q.1- Create a class Animal as a base class and define method animal_attribute. Create another class Tiger which is inheriting Animal and access the base class method.
+
+class  
